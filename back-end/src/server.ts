abc6e49d5b04fe2336796464cf2954cd8aa08dae
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-04-16 17:05:47 +0000

    fix: add cors origin

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: "*"}));
 
 app.get("/add", async (req: Request, res: Response) => {
   try {
